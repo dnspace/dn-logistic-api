34a7c2e27a9ef77e09f1de7a830564445ae05c17
dnspace <dnspace3d@gmail.com>
2018-11-22 12:52:10 +0700

    Update endpoint.php

--- application/config/endpoint.php
+++ application/config/endpoint.php
@@ -2,26 +2,26 @@
 
 if(file_exists(FCPATH.'local.txt')) {
 	// Local Server
-	define('master',   			"http://localhost:8080/dn-logistic-api/");
-	define('master-front',  	"http://localhost:8080/e-logistic/");
+	define('master',   			"https://202.158.125.181:4599/");
+	define('master-front',  	"https://202.158.125.181:4566/");
     $config['emailsender']	= "noreplyplease@service-division.com";
     $config['emailfrom']	= "E-Logistic System";
 }elseif(file_exists(FCPATH.'dev.txt')) {
 	// Development Server
-	define('master',   			"http://localhost:8080/dn-logistic-api/");
-	define('master-front',  	"http://localhost:8080/e-logistic/");
+	define('master',   			"https://202.158.125.181:4599/");
+	define('master-front',  	"https://202.158.125.181:4566/");
     $config['emailsender']	= "noreplyplease@service-division.com";
     $config['emailfrom']	= "E-Logistic System";
 }elseif(file_exists(FCPATH.'stg.txt')) {
 	// Staging Server
-	define('master',   			"http://localhost:8080/dn-logistic-api/");
-	define('master-front',  	"http://localhost:8080/e-logistic/");
+	define('master',   			"https://202.158.125.181:4599/");
+	define('master-front',  	"https://202.158.125.181:4566/");
     $config['emailsender']	= "noreplyplease@service-division.com";
     $config['emailfrom']	= "E-Logistic System";
 }else{
 	// Production Server
-	define('master',   			"http://localhost:8080/dn-logistic-api/");
-	define('master-front',  	"http://localhost:8080/e-logistic/");
+	define('master',   			"https://202.158.125.181:4599/");
+	define('master-front',  	"https://202.158.125.181:4566/");
     $config['emailsender']	= "noreplyplease@service-division.com";
     $config['emailfrom']	= "E-Logistic System";
 }
